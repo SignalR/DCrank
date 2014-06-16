ec8f419d20337b57e7ce93380c4572f58172c81a
intern-felicco <t-fetone@microsoft.com>
2014-06-16 16:47:56 -0700

    Updates based on code review and a sit down with Fowler

--- src/Microsoft.AspNet.SignalR.DCrank.TestController/ControllerHub.cs
+++ src/Microsoft.AspNet.SignalR.DCrank.TestController/ControllerHub.cs
@@ -1,52 +1,35 @@
 ﻿
+using System.Threading.Tasks;
 namespace Microsoft.AspNet.SignalR.DCrank.TestController
 {
     public class ControllerHub : Hub
     {
-        public void Hello()
-        {
-            Clients.All.hello();
-        }
 
-        // Call the broadcastMessage method to the UI with the appropriate message.
-        public void Send( string connectionId)
+        public override async Task OnConnected()
         {
-            Clients.All.broadcastMessage(connectionId + "has connected");
-        }
-
-        public void AgentsAlive(int x)
-        {
-            Clients.All.pong(x);
+            var isUI = Context.Request.QueryString["UI"]=="1";
+            if (isUI)
+            {
+                await Groups.Add(Context.ConnectionId, "UIIdentifier");
+            }
         }
 
         //A test to see if there is end to end communication - see if the int you send is actually received
-        public void Ping(int x)
+        public void Ping(int value)
         {
-            Clients.All.broadcastMessage(x);
+            Clients.All.pong(value);
         }
 
-        //Allows you to broadcast to a specific client on the hub - future method not yet used
-        public void RespondToClient(string connectionId)
+        public void AgentAlive(int value)
         {
-            GetClients(connectionId).Send();
-        }
+            Clients.Group("UIIdentifier").pongResponse(Context.ConnectionId, value);
 
-        //Establishes a way to identify a specific client on the hub - future method not yet used
-        private dynamic GetClients(string conId = "")
-        {
-            var clients = GlobalHost.ConnectionManager.GetHubContext<ControllerHub>().Clients;
-            if (conId == "")
-            {
-                return clients;
-            }
-
-            return clients.User(conId);
         }
 
         //Want to use this to recieve messages from the Agent - has to be called from the agent - A test of end to end connectivity
-        public void AgentHeartbeat(string connectionId)
+        public void AgentHeartbeat()
         {
-            Send(connectionId);
+            Clients.Group("UIIdentifier").agentConnected(Context.ConnectionId);
         }
     }
 }
