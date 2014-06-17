df18aa7cab09461951787ad27bb85b60b7b9df50
intern-felicco <t-fetone@microsoft.com>
2014-06-17 16:42:17 -0700

    More Code Review Changes

--- src/Microsoft.AspNet.SignalR.DCrank.TestController/ControllerHub.cs
+++ src/Microsoft.AspNet.SignalR.DCrank.TestController/ControllerHub.cs
@@ -4,12 +4,13 @@ namespace Microsoft.AspNet.SignalR.DCrank.TestController
 {
     public class ControllerHub : Hub
     {
+        private readonly string uiGroup = "Dashboard";
         public override async Task OnConnected()
         {
             var isDashboard = Context.Request.QueryString["UI"] == "1";
             if (isDashboard)
             {
-                await Groups.Add(Context.ConnectionId, "Dashboard");
+                await Groups.Add(Context.ConnectionId, uiGroup);
             }
         }
 
@@ -21,14 +22,13 @@ namespace Microsoft.AspNet.SignalR.DCrank.TestController
 
         public void AgentAlive(int value)
         {
-            Clients.Group("Dashboard").pongResponse(Context.ConnectionId, value);
-
+            Clients.Group(uiGroup).pongResponse(Context.ConnectionId, value);
         }
 
         //Want to use this to recieve messages from the Agent - has to be called from the agent - A test of end to end connectivity
         public void AgentHeartbeat()
         {
-            Clients.Group("Dashboard").agentConnected(Context.ConnectionId);
+            Clients.Group(uiGroup).agentConnected(Context.ConnectionId);
         }
     }
 }
