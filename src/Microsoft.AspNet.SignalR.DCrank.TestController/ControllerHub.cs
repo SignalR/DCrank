063d6f4fe5ef43ef371fbe950a2db310173c9510
intern-felicco <t-fetone@microsoft.com>
2014-06-17 10:31:45 -0700

    Changes based on Code Review feedback

--- src/Microsoft.AspNet.SignalR.DCrank.TestController/ControllerHub.cs
+++ src/Microsoft.AspNet.SignalR.DCrank.TestController/ControllerHub.cs
@@ -4,32 +4,31 @@ namespace Microsoft.AspNet.SignalR.DCrank.TestController
 {
     public class ControllerHub : Hub
     {
-
         public override async Task OnConnected()
         {
-            var isUI = Context.Request.QueryString["UI"]=="1";
-            if (isUI)
+            var isDashboard = Context.Request.QueryString["UI"] == "1";
+            if (isDashboard)
             {
-                await Groups.Add(Context.ConnectionId, "UIIdentifier");
+                await Groups.Add(Context.ConnectionId, "Dashboard");
             }
         }
 
         //A test to see if there is end to end communication - see if the int you send is actually received
         public void Ping(int value)
         {
-            Clients.All.pong(value);
+            Clients.All.ping(value);
         }
 
         public void AgentAlive(int value)
         {
-            Clients.Group("UIIdentifier").pongResponse(Context.ConnectionId, value);
+            Clients.Group("Dashboard").pongResponse(Context.ConnectionId, value);
 
         }
 
         //Want to use this to recieve messages from the Agent - has to be called from the agent - A test of end to end connectivity
         public void AgentHeartbeat()
         {
-            Clients.Group("UIIdentifier").agentConnected(Context.ConnectionId);
+            Clients.Group("Dashboard").agentConnected(Context.ConnectionId);
         }
     }
 }
