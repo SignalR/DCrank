fcd64435f4136fac38cf0ade371fa0015bf51f24
intern-felicco <t-fetone@microsoft.com>
2014-08-14 16:02:39 -0700

    clean up of disconnect method

--- src/Microsoft.AspNet.SignalR.DCrank.Crank/Worker.cs
+++ src/Microsoft.AspNet.SignalR.DCrank.Crank/Worker.cs
@@ -91,7 +91,7 @@ namespace Microsoft.AspNet.SignalR.DCrank.Crank
                             break;
 
                         case "stop":
-                            _targetConnectionCount = 0;
+                            targetConnectionCount = 0;
                             foreach (var client in _clients)
                             {
                                 client.StopConnection();
@@ -101,7 +101,6 @@ namespace Microsoft.AspNet.SignalR.DCrank.Crank
                             Log("Connections stopped succesfully");
 
                             workerStopped = true;
-                            targetConnectionCount = 0;
                             break;
                     }
                 }
