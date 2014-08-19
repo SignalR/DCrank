208275d88239ad410fd8d0c17267f42bc8b12376
intern-felicco <t-fetone@microsoft.com>
2014-08-19 14:36:57 -0700

    add performance counters to the UI

--- src/Microsoft.AspNet.SignalR.DCrank.TestController/ControllerHub.cs
+++ src/Microsoft.AspNet.SignalR.DCrank.TestController/ControllerHub.cs
@@ -1,5 +1,7 @@
 ﻿
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Threading;
 using System.Threading.Tasks;
 namespace Microsoft.AspNet.SignalR.DCrank.TestController
@@ -7,7 +9,9 @@ namespace Microsoft.AspNet.SignalR.DCrank.TestController
     public class ControllerHub : Hub
     {
         private readonly string _uiGroup = "Dashboard";
-        private readonly int numberOfWorkersPerAgent = 3; // Default value - can be changed/made configurable as needed
+        private readonly int _numberOfWorkersPerAgent = 3; // Default value - can be changed/made configurable as needed
+        private string _connectionString;
+        private SqlConnection _performanceDatabaseConnection;
 
         public override async Task OnConnected()
         {
@@ -36,6 +40,16 @@ namespace Microsoft.AspNet.SignalR.DCrank.TestController
         public void PongWorker(int workerId, int value)
         {
             Clients.All.workerPongResponse(Context.ConnectionId, workerId, value);
+        }
+
+        public void LogAgent(string message)
+        {
+            Clients.All.agentsLog(Context.ConnectionId, message);
+        }
+
+        public void LogWorker(int workerId, string message)
+        {
+            Clients.All.workersLog(Context.ConnectionId, workerId, message);
         }
 
         public void AgentHeartbeat(object heartbeatInformation)
@@ -78,9 +92,12 @@ namespace Microsoft.AspNet.SignalR.DCrank.TestController
             int numberOfAgents = agentIdList.Length;
             if (numberOfConnections != 0 && numberOfAgents != 0)
             {
-                int numberOfConnectionsPerWorker = numberOfConnections / (numberOfWorkersPerAgent * numberOfAgents);
-                Clients.All.startWorkers(targetAddresss, numberOfWorkersPerAgent, numberOfConnectionsPerWorker);
+                int numberOfConnectionsPerWorker = numberOfConnections / (_numberOfWorkersPerAgent * numberOfAgents);
+                Clients.All.startWorkers(targetAddresss, _numberOfWorkersPerAgent, numberOfConnectionsPerWorker);
             }
+
+            // get connection string from the database
+            // Clients.All.requestConnectionString();
         }
 
         public void StartTest(int messageSize, int messageRate)
@@ -93,14 +110,53 @@ namespace Microsoft.AspNet.SignalR.DCrank.TestController
             Clients.All.killConnections();
         }
 
-        public void LogAgent(string message)
+        public void SetConnectionString(string connectionString)
         {
-            Clients.All.agentsLog(Context.ConnectionId, message);
+            _connectionString = connectionString;
         }
 
-        public void LogWorker(int workerId, string message)
+        public void ConnectToDatabase()
         {
-            Clients.All.workersLog(Context.ConnectionId, workerId, message);
+            _performanceDatabaseConnection = new SqlConnection(_connectionString);
+
+            // Allows the connection to be established before trying to open it
+            Thread.Sleep(15000);
+
+            try
+            {
+                _performanceDatabaseConnection.Open();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public void GetPerformanceData()
+        {
+            try
+            {
+                List<List<string>> perfermanceData = new List<List<string>>();
+
+                SqlDataReader myReader = null;
+                SqlCommand myCommand = new SqlCommand("select * from table",
+                                                         _performanceDatabaseConnection);
+                myReader = myCommand.ExecuteReader();
+                // Reads through the entire table once
+                while (myReader.Read())
+                {
+                    List<string> currentRow = new List<string>();
+                    currentRow.Add(myReader["Column1"].ToString());
+                    currentRow.Add(myReader["Column2"].ToString());
+                    perfermanceData.Add(currentRow);
+                }
+
+                Clients.All.updatePerfCounters(perfermanceData);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
