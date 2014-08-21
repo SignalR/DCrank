9a524b00725b6a15abd1b76f2cc86f5d647fef21
intern-felicco <t-fetone@microsoft.com>
2014-08-21 16:48:50 -0700

    Performance counters are now displayed on the UI

---
+++ src/Microsoft.AspNet.SignalR.LoadTestHarness/Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNet.SignalR.LoadTestHarness;
+using Microsoft.Owin;
+using Microsoft.AspNet.SignalR.DCrank.PerfCounterHarness;
+using Owin;
+using System.Configuration;
+
+[assembly: OwinStartup(typeof(Startup))]
+
+namespace Microsoft.AspNet.SignalR.LoadTestHarness
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["PerformanceCounters"];
+            GlobalHost.Configuration.DisconnectTimeout = TimeSpan.FromSeconds(60);
+            GlobalHost.DependencyResolver.AddDCrankHarness(connectionString.ConnectionString);
+
+            app.MapSignalR<TestConnection>("/TestConnection");
+            app.MapSignalR();
+
+            app.UseDCrankEndpoint();
+
+            Dashboard.Init();
+        }
+    }
+}
