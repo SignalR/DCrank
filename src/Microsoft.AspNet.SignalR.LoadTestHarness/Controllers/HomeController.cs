c5f092504b94b3668279f370d3218e234cb97fc7
mikary <mikary@microsoft.com>
2014-09-11 10:49:21 -0700

    Moved index.html into MVC view

--- src/Microsoft.AspNet.SignalR.LoadTestHarness/Controllers/HomeController.cs
+++ src/Microsoft.AspNet.SignalR.LoadTestHarness/Controllers/HomeController.cs
@@ -11,19 +11,5 @@ namespace Microsoft.AspNet.SignalR.LoadTestHarness.Controllers
         {
             return View();
         }
-
-        public IActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
