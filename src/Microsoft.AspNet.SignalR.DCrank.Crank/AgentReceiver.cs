e6925a9728a7a812aa216b92d7d2414190478fde
mikary <mikary@microsoft.com>
2014-10-01 09:30:31 -0700

    Created AgentReceiver class and unit tests.

---
+++ src/Microsoft.AspNet.SignalR.DCrank.Crank/AgentReceiver.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Microsoft.AspNet.SignalR.DCrank.Crank
+{
+    public class AgentReceiver
+    {
+        private readonly StreamReader _reader;
+        private readonly IAgent _agent;
+
+        public AgentReceiver(StreamReader reader, IAgent agent)
+        {
+            _reader = reader;
+            _agent = agent;
+        }
+
+        public void Start()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    var messageString = await _reader.ReadLineAsync();
+                    try
+                    {
+                        var message = JsonConvert.DeserializeObject<Message>(messageString);
+
+                        switch (message.Command.ToLowerInvariant())
+                        {
+                            case "pong":
+                                await _agent.Pong(
+                                    message.Value["Id"].ToObject<int>(),
+                                    message.Value["Value"].ToObject<int>());
+                                break;
+                            case "log":
+                                await _agent.Log(
+                                    message.Value["Id"].ToObject<int>(),
+                                    message.Value["Text"].ToObject<string>());
+                                break;
+                            case "status":
+                                await _agent.Status(
+                                    message.Value["Id"].ToObject<int>(),
+                                    message.Value["StatusInformation"].ToObject<StatusInformation>());
+                                break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Trace.WriteLine(ex.Message);
+                    }
+                }
+            });
+        }
+    }
+}
---
+++ tests/Microsoft.AspNet.SignalR.DCrank.Crank.Tests/AgentReceiverUnitTests.cs
@@ -0,0 +1,162 @@
+﻿using System.IO;
+using System.Threading;
+using Moq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Xunit;
+
+namespace Microsoft.AspNet.SignalR.DCrank.Crank
+{
+    public class AgentReceiverUnitTests
+    {
+        [Fact]
+        public void CallsPongMethodWhenPongMessageIsWrittenToStream()
+        {
+            // arrange
+            var pongObject = new
+            {
+                Id = 50,
+                Value = 60
+            };
+
+            var message = new Message
+            {
+                Command = "pong",
+                Value = JToken.FromObject(pongObject)
+            };
+
+            var stream = new MemoryStream();
+            var streamWriter = new StreamWriter(stream);
+
+            streamWriter.WriteLine(JsonConvert.SerializeObject(message));
+            streamWriter.Flush();
+            stream.Position = 0;
+
+            var pongId = 0;
+            var pongValue = 0;
+            var pongCts = new CancellationTokenSource();
+
+            var agent = new Mock<IAgent>();
+            agent.Setup(instance => instance.Pong(It.IsAny<int>(), It.IsAny<int>()))
+                .Callback<int, int>((id, value) =>
+                {
+                    pongId = id;
+                    pongValue = value;
+                    pongCts.Cancel();
+                });
+
+            var streamReader = new StreamReader(stream);
+            var agentReceiver = new AgentReceiver(streamReader, agent.Object);
+
+            // act
+            agentReceiver.Start();
+
+            // assert
+            Assert.True(pongCts.Token.WaitHandle.WaitOne(1000));
+            Assert.Equal(pongObject.Id, pongId);
+            Assert.Equal(pongObject.Value, pongValue);
+        }
+
+        [Fact]
+        public void CallsLogMethodWhenLogMessageIsWrittenToStream()
+        {
+            // arrange
+            var logObject = new
+            {
+                Id = 70,
+                Text = "Log message"
+            };
+
+            var message = new Message
+            {
+                Command = "log",
+                Value = JToken.FromObject(logObject)
+            };
+
+            var stream = new MemoryStream();
+            var streamWriter = new StreamWriter(stream);
+
+            streamWriter.WriteLine(JsonConvert.SerializeObject(message));
+            streamWriter.Flush();
+            stream.Position = 0;
+
+            var logId = 0;
+            var logText = "";
+            var logCts = new CancellationTokenSource();
+
+            var agent = new Mock<IAgent>();
+            agent.Setup(instance => instance.Log(It.IsAny<int>(), It.IsAny<string>()))
+                .Callback<int, string>((id, text) =>
+                {
+                    logId = id;
+                    logText = text;
+                    logCts.Cancel();
+                });
+
+            var streamReader = new StreamReader(stream);
+            var agentReceiver = new AgentReceiver(streamReader, agent.Object);
+
+            // act
+            agentReceiver.Start();
+
+            // assert
+            Assert.True(logCts.Token.WaitHandle.WaitOne(1000));
+            Assert.Equal(logObject.Id, logId);
+            Assert.Equal(logObject.Text, logText);
+        }
+
+        [Fact]
+        public void CallsStatusMethodWhenStatusMessageIsWrittenToStream()
+        {
+            // arrange
+            var statusObject = new
+            {
+                Id = 80,
+                StatusInformation = new StatusInformation
+                {
+                    ConnectedCount = 90,
+                    DisconnectedCount = 100,
+                    ReconnectingCount = 110,
+                    TargetConnectionCount = 120
+                }
+            };
+
+            var message = new Message
+            {
+                Command = "status",
+                Value = JToken.FromObject(statusObject)
+            };
+
+            var stream = new MemoryStream();
+            var streamWriter = new StreamWriter(stream);
+
+            streamWriter.WriteLine(JsonConvert.SerializeObject(message));
+            streamWriter.Flush();
+            stream.Position = 0;
+
+            var statusId = 0;
+            StatusInformation statusInformation = null;
+            var statusCts = new CancellationTokenSource();
+
+            var agent = new Mock<IAgent>();
+            agent.Setup(instance => instance.Status(It.IsAny<int>(), It.IsAny<StatusInformation>()))
+                .Callback<int, StatusInformation>((id, status) =>
+                {
+                    statusId = id;
+                    statusInformation = status;
+                    statusCts.Cancel();
+                });
+
+            var streamReader = new StreamReader(stream);
+            var agentReceiver = new AgentReceiver(streamReader, agent.Object);
+
+            // act
+            agentReceiver.Start();
+
+            // assert
+            Assert.True(statusCts.Token.WaitHandle.WaitOne(1000));
+            Assert.Equal(statusObject.Id, statusId);
+            Assert.Equal(JsonConvert.SerializeObject(statusObject.StatusInformation), JsonConvert.SerializeObject(statusInformation));
+        }
+    }
+}
