81d894bdefa430da68bbd0fa38d62213b567a042
Abhishek Nanda <abnanda@microsoft.com>
2014-11-06 18:06:40 -0800

    Code review changes

--- azure-packages/CrankDashboardServiceProject/dcrank-dashboard.ps1
+++ azure-packages/CrankDashboardServiceProject/dcrank-dashboard.ps1
@@ -1,4 +1,3 @@
-param([string]$AzurePublishSettingsFile, [string]$AffinityGroupName, [string]$Location, [string]$StorageAccountName, [string]$ServiceName)
+param([string]$AzurePublishSettingsFile, [string]$AffinityGroupName, [string]$StorageAccountName, [string]$ServiceName)
 Import-AzurePublishSettingsFile $AzurePublishSettingsFile
-#New-AzureAffinityGroup -Name $AffinityGroupName -Location $Location
 Publish-AzureServiceProject -ServiceName $ServiceName -AffinityGroup $AffinityGroupName -StorageAccountName $StorageAccountName
--- azure-packages/CrankWorkerServiceProject/dcrank-agent.ps1
+++ azure-packages/CrankWorkerServiceProject/dcrank-agent.ps1
@@ -1,6 +1,5 @@
-param([string]$AzurePublishSettingsFile, [string]$AffinityGroupName, [string]$StorageAccountName, [string]$Location, [string]$ServiceName)
+param([string]$AzurePublishSettingsFile, [string]$AffinityGroupName, [string]$StorageAccountName, [string]$ServiceName)
 Import-AzurePublishSettingsFile $AzurePublishSettingsFile
-#New-AzureAffinityGroup -Name $AffinityGroupName -Location $Location
 Publish-AzureServiceProject -ServiceName $ServiceName -AffinityGroup $AffinityGroupName -StorageAccountName $StorageAccountName
 
 
